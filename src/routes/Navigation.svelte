46127228ec7fdd91b21cb16f3550a30ca7a1741a
supra <supra1302@protonmail.com>
2025-01-10 22:50:26 +0000

    added dark mode toggle

--- src/routes/Navigation.svelte
+++ src/routes/Navigation.svelte
@@ -2,6 +2,12 @@
 	import { page } from "$app/state"
 	import { cn } from "$lib/utils"
 
+	import Sun from "lucide-svelte/icons/sun"
+	import Moon from "lucide-svelte/icons/moon"
+
+	import { toggleMode } from "mode-watcher"
+	import { Button } from "$lib/components/ui/button/index.js"
+
 	// Props for mobile navigation
 	let { isMobile = false, onClose }: { isMobile?: boolean; onClose?: () => void } = $props()
 
@@ -39,5 +45,14 @@
 				</a>
 			</li>
 		{/each}
+		<Button onclick={toggleMode} variant="outline" size="icon">
+			<Sun
+				class="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0"
+			/>
+			<Moon
+				class="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100"
+			/>
+			<span class="sr-only">Toggle theme</span>
+		</Button>
 	</ul>
 </nav>
